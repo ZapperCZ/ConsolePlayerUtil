de9d1f6de28197ab7be6ce632fa99799cbf22388
ZapperCZ <janfridl@seznam.cz>
2022-04-11 00:07:05 +0200

    sum changes, is kinda workin now, audio path hardcoded atm

---
+++ ConsolePlayerUtil/FastConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsolePlayerUtil
+{
+    class FastConsole
+    {
+        static readonly BufferedStream str;
+        static FastConsole()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            str = new BufferedStream(Console.OpenStandardOutput(), 0x15000);
+        }
+        public static void WriteLine(String s) => Write(s + "\r\n");
+        public static void Write(String s)
+        {
+            // avoid endless 'GetByteCount' dithering in 'Encoding.Unicode.GetBytes(s)'
+            var rgb = new byte[s.Length << 1];
+            Encoding.Unicode.GetBytes(s, 0, s.Length, rgb, 0);
+
+            lock (str)   // (optional, can omit if appropriate)
+                str.Write(rgb, 0, rgb.Length);
+        }
+        public static void Flush() { lock (str) str.Flush(); }
+    }
+}
--- ConsolePlayerUtil/Program.cs
+++ ConsolePlayerUtil/Program.cs
@@ -1,27 +1,238 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection;
+using System.Media;
 
 namespace ConsolePlayerUtil
 {
     class Program
     {
+        enum FileExtensions
+        {
+            jpg,
+            png
+        }
+        static FileExtensions selectedExtension = new FileExtensions();
+        static string selectedDirectory;
+        static FileInfo[] selectedFiles;
+        static SoundPlayer audio = new SoundPlayer();
+        static List<Bitmap> SelectedBitmaps = new List<Bitmap>();
+        static List<string> PlayableDirectories;
         static void Main(string[] args)
         {
             HandleInput();
+            audio.SoundLocation = "D:\\Bordel\\Rust screen cringe\\badapple\\bad apple - orig.wav";
+            Console.Write("Loading files");
+            selectedFiles = new DirectoryInfo(selectedDirectory).GetFiles("*." + selectedExtension.ToString());
+            int writeInterval = selectedFiles.Length / 10;
+            for (int i = 0; i < selectedFiles.Length; i++)
+            {
+                Bitmap newBitmap = new Bitmap(selectedFiles[i].FullName);
+                SelectedBitmaps.Add(newBitmap);
+                if (i % writeInterval == 0)
+                    Console.Write(".");
+            }
+            ImageFormat selectedFormat = ImageFormat.Png;
+            switch (selectedExtension)
+            {
+                case FileExtensions.png:
+                    selectedFormat = ImageFormat.Png;
+                    break;
+                case FileExtensions.jpg:
+                    selectedFormat = ImageFormat.Jpeg;
+                    break;
+            }
+            Console.Clear();
+            Console.Write("Commencing playback");
+            Thread.Sleep(500);
+            PlayBitmaps(SelectedBitmaps,selectedFormat,30,30,0);
             Console.ReadLine();
         }
         static bool HandleInput()  //Returns true if all input is valid, otherwise returns false
         {
-            //Check and assign input arguments
-            Console.WriteLine("Console Player Util");
-            Console.WriteLine(choiceMultiple("Play files from [C]urrent folder | Play files from [D]irectory", new char[] { 'C', 'D' }));
-
+            selectedDirectory = "";
+            PlayableDirectories = new List<string>();
+            Console.Write("Welcome to Console Player Util\nYou can use arrows for menu orientation and Enter to confirm.\nPress Enter to start");
+            Console.ReadLine();
+            selectedExtension = (FileExtensions)ArrowChoice("Select file type",new string[] {".jpg",".png"});
+            Console.Write(selectedExtension + " selected");
+            Thread.Sleep(800);
+            while (true)
+            {
+                PlayableDirectories = new List<string>();
+                Console.Clear();
+                if (ArrowChoice("Select file location", new string[] { "Current directory", "Custom directory" }) == 0)
+                {
+                    string currDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    DirectoryInfo[] di = new DirectoryInfo(currDir).GetDirectories("*.*", SearchOption.AllDirectories);
+                    foreach (DirectoryInfo d in di)
+                    {
+                        FileInfo[] currDirFiles = d.GetFiles("*" + selectedExtension.ToString());
+                        if (currDirFiles.Length > 0)
+                        {
+                            PlayableDirectories.Add(d.FullName.Replace(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), ""));
+                        }
+                    }
+                    if (PlayableDirectories.Count == 0)
+                    {
+                        Console.WriteLine("No folder containing the selected file type found.");
+                        Thread.Sleep(1000);
+                        continue;
+                    }
+                    selectedDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + PlayableDirectories[ArrowChoice("Select playback folder", PlayableDirectories.ToArray())];
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.Write("Input desired folder path: ");
+                    string path = Console.ReadLine();
+                    if (Directory.Exists(path))
+                    {
+                        DirectoryInfo[] di = new DirectoryInfo(path).GetDirectories("*.*", SearchOption.AllDirectories);
+                        foreach (DirectoryInfo d in di)
+                        {
+                            FileInfo[] currDirFiles = d.GetFiles("*" + selectedExtension.ToString());
+                            if (currDirFiles.Length > 0)
+                            {
+                                PlayableDirectories.Add(d.FullName.Replace(path,""));
+                            }
+                        }
+                        if (PlayableDirectories.Count == 0)
+                        {
+                            Console.WriteLine("No folder containing the selected file type found.");
+                            Thread.Sleep(1000);
+                            continue;
+                        }
+                        selectedDirectory = path + PlayableDirectories[ArrowChoice("Select playback folder", PlayableDirectories.ToArray())];
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input path was not found.");
+                        Thread.Sleep(1000);
+                        continue;
+                    }
+                }
+            }
             return true;
         }
-        static int choiceMultiple(string question, char[] selectableCharacters)
+        static void PlayBitmaps(List<Bitmap> inputBitmaps, ImageFormat imageFormat, int originalFPS, int targetFPS, int skippedFrames)
+        {
+            Console.Clear();
+            Bitmap currFrame;
+            int frameTime = 1000 / targetFPS;
+            float frameTimeRemainder = (1000f / (float)targetFPS) - frameTime;
+            int leapFrameIndex = 0;
+            int leapMs;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (Math.Round(frameTimeRemainder * i, 2) >= 1)
+                {
+                    leapFrameIndex = i;
+                    break;
+                }
+            }
+            DateTime nextFrameTime = DateTime.Now.AddMilliseconds(1000 / targetFPS);
+            audio.Play();
+            for (int i = 0; i < inputBitmaps.Count; i++)
+            {
+                leapMs = 0;
+                if (skippedFrames > 0 && i % skippedFrames != 0)
+                {
+                    continue;
+                }
+                if (leapFrameIndex > 0 && i % leapFrameIndex == 0)
+                {
+                    leapMs = 1;
+                }
+                currFrame = inputBitmaps[i];
+                for (int x = 0; x < inputBitmaps[0].Height * 2; x++)
+                {
+                    FastConsole.WriteLine("");
+                }
+                char c;
+                Color currPixel;
+                for (int y = 0; y < currFrame.Height; y++)
+                {
+                    for (int x = 0; x < currFrame.Width; x++)
+                    {
+                        currPixel = inputBitmaps[i].GetPixel(x, y);
+                        if (currPixel.A == 0)
+                            c = ' ';
+                        else
+                            c = CharacterGrayScale(currPixel.R);
+                        FastConsole.Write(c.ToString() + " ");
+                    }
+                    FastConsole.WriteLine("");
+                }
+                DateTime currTime;
+                FastConsole.Flush();
+                while (true)
+                {
+                    currTime = DateTime.Now;
+                    if ((nextFrameTime - currTime).Milliseconds == 0)
+                    {
+                        nextFrameTime = currTime.AddMilliseconds((1000 / targetFPS) + 1 + leapMs);  //No clue why the additional ms is needed, but it just works with it and I'm leaving it as is
+                        break;
+                    }
+                }
+            }
+        }
+        static int ArrowChoice(string output, string[] items)
+        {
+            Console.CursorVisible = false;
+            int selection = 0;
+            int maxItemLength = 0;
+            string item;
+            string padding;
+            char selectedChar = ' ';
+            bool userSelected = false;
+            foreach(string s in items)
+            {
+                maxItemLength = s.Length > maxItemLength ? s.Length : maxItemLength;
+            }
+            while (!userSelected)
+            {
+                Console.Clear();
+                Console.Write(output);
+                for (int i = 0; i < items.Length; i++)
+                {
+                    item = items[i];
+                    selectedChar = selection == i ? '■' : ' ';
+                    padding = "{0,-" + (maxItemLength + 4) + "}"+"{1}";
+                    Console.Write(padding,"\n" + item, selectedChar);
+                }
+                ConsoleKey inputKey = Console.ReadKey().Key;
+                switch (inputKey)
+                {
+                    case ConsoleKey.UpArrow:
+                        selection--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selection++;
+                        break;
+                    case ConsoleKey.Enter:
+                        userSelected = true;
+                        break;
+                }
+                LimitValue(ref selection, 0, items.Length - 1);
+            }
+            Console.CursorVisible = true;
+            Console.Clear();
+            return selection;
+        }
+        static void LimitValue(ref int value, int lowerLimit, int upperLimit)
+        {
+            value = (value < lowerLimit) ? lowerLimit : value;
+            value = (value > upperLimit) ? upperLimit : value;
+        }
+        static int ChoiceMultiple(string question, char[] selectableCharacters)
         {
             int selection = -1;
             for(int i = 0; i < selectableCharacters.Length; i++)
@@ -42,7 +253,7 @@ namespace ConsolePlayerUtil
             }
             return selection;
         }
-        static bool choiceYesNo(string question)
+        static bool ChoiceYesNo(string question)
         {
             bool answer;
             string choice;
@@ -67,7 +278,7 @@ namespace ConsolePlayerUtil
             }
             return answer;
         }
-        static char characterGrayScale(int grayScale)
+        static char CharacterGrayScale(int grayScale)
         {
             string grayscaleMap = "@%#&+=-:.";
             //string grayscaleMap = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.";
